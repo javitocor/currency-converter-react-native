6b4538e1c979b9f91311286da842066a3134affe
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-19 14:06:24 +0200

    adding flatlistto currency screen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -99,7 +99,9 @@ export default ({ navigation }) => {
             <ConversionInput
               text={baseCurrency}
               value="123"
-              onButtonPress={() => alert('todo!')}
+              onButtonPress={() =>
+                navigation.push('CurrencyList', { title: 'Base Currency' })
+              }
               keyboardType="numeric"
               onChangeText={(text) => console.log('text', text)}
             />
@@ -107,7 +109,9 @@ export default ({ navigation }) => {
               text={quoteCurrency}
               value="123"
               editable={false}
-              onButtonPress={() => alert('todo!')}
+              onButtonPress={() =>
+                navigation.push('CurrencyList', { title: 'Quote Currency' })
+              }
             />
           </View>
           <Text style={styles.text}>
