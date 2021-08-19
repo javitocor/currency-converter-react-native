a9a815ca19b4e9ffebde0834c28d0e79f3d329c8
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-19 14:15:59 +0200

    designatecurrent currency

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,32 +1,129 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  Image,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { format } from 'date-fns';
+import { Entypo } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import Home from '../screens/Home';
-import Options from '../screens/Options';
-import CurrencyList from '../screens/CurrencyList';
+import colors from '../constants/colors';
+import { ConversionInput } from '../components/ConversionInput';
+import { Button } from '../components/Button';
+import { KeyboardSpacer } from '../components/KeyboardSpacer';
 
-const MainStack = createStackNavigator();
-const MainStackScreen = () => (
-  <MainStack.Navigator>
-    <MainStack.Screen
-      name="Home"
-      component={Home}
-      options={{ headerShown: false }}
-    />
-    <MainStack.Screen name="Options" component={Options} />
-    <MainStack.Screen
-      name="CurrencyList"
-      component={CurrencyList}
-      options={({ route }) => ({
-        title: route.params.title,
-      })}
-    />
-  </MainStack.Navigator>
-);
+const screen = Dimensions.get('window');
 
-export default () => (
-  <NavigationContainer>
-    <MainStackScreen />
-  </NavigationContainer>
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.blue,
+  },
+  content: {
+    paddingTop: screen.height * 0.1,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  logoBackground: {
+    width: screen.width / 0.45,
+    height: screen.width * 0.45,
+  },
+  logo: {
+    position: 'absolute',
+    width: screen.width * 0.25,
+    height: screen.width * 0.25,
+  },
+  textHeader: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 14,
+    color: colors.white,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  header: {
+    alignItems: 'flex-end',
+    marginHorizontal: 20,
+  },
+});
+
+export default ({ navigation }) => {
+  const baseCurrency = 'USD';
+  const quoteCurrency = 'GBP';
+  const conversionRate = 0.89824;
+  const date = '2020-03-23';
+
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <ScrollView scrollEnabled={scrollEnabled}>
+        <SafeAreaView style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.push('Options')}>
+            <Entypo name="cog" size={32} color={colors.white} />
+          </TouchableOpacity>
+        </SafeAreaView>
+
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={require('../assets/images/background.png')}
+              style={styles.logoBackground}
+              resizeMode="contain"
+            />
+            <Image
+              source={require('../assets/images/logo.png')}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+          </View>
+          <Text style={styles.textHeader}>Currency Converter</Text>
+          <View style={styles.inputContainer}>
+            <ConversionInput
+              text={baseCurrency}
+              value="123"
+              onButtonPress={() =>
+                navigation.push('CurrencyList', { title: 'Base Currency' })
+              }
+              keyboardType="numeric"
+              onChangeText={(text) => console.log('text', text)}
+            />
+            <ConversionInput
+              text={quoteCurrency}
+              value="123"
+              editable={false}
+              onButtonPress={() =>
+                navigation.push('CurrencyList', { title: 'Quote Currency' })
+              }
+            />
+          </View>
+          <Text style={styles.text}>
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(
+              new Date(date),
+              'MMM do, yyyy'
+            )}`}
+          </Text>
+          <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
+          <KeyboardSpacer onToggle={(visible) => setScrollEnabled(visible)} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
