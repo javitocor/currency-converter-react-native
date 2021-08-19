92194c46ccafe83c554d760673f0cb5c3f4a5e06
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-19 10:51:12 +0200

    add icons to Options screen

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -1,4 +1,5 @@
 export default {
   text: "#343434",
-  border:"#E2E2E2"
+  border:"#E2E2E2",
+  blue:"#4f6d7a"
 };
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Entypo }from '@expo/vector-icons';
 import colors from '../constants/colors';
 
 const styles = StyleSheet.create({
   row:{
     paddingHorizontal: 20,
     paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "space-between",
   },
   text:{
     fontSize: 16,
@@ -23,18 +27,21 @@ export default () => {
     <SafeAreaView>
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>Themes</Text>
+        <Entypo name="chevron-right" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basics</Text>
+        <Entypo name="export" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native by Example</Text>
+        <Entypo name="export" size={20} color={colors.blue} />
       </TouchableOpacity>
     </SafeAreaView>
   );
