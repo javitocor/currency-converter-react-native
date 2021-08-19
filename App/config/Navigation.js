dded5533ad7a9a6bc80a17748de139ded5b33428
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-19 17:33:40 +0200

    modify navigation

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,129 +1,55 @@
-import React, { useState } from 'react';
-import {
-  View,
-  StyleSheet,
-  StatusBar,
-  Dimensions,
-  Image,
-  Text,
-  ScrollView,
-  TouchableOpacity,
-} from 'react-native';
-import { format } from 'date-fns';
-import { Entypo } from '@expo/vector-icons';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
 
-import colors from '../constants/colors';
-import { ConversionInput } from '../components/ConversionInput';
-import { Button } from '../components/Button';
-import { KeyboardSpacer } from '../components/KeyboardSpacer';
+import Home from "../screens/Home";
+import Options from "../screens/Options";
+import CurrencyList from "../screens/CurrencyList";
+import colors from "../constants/colors";
 
-const screen = Dimensions.get('window');
+const MainStack = createStackNavigator();
+const MainStackScreen = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen
+      name="Home"
+      component={Home}
+      options={{ headerShown: false }}
+    />
+    <MainStack.Screen name="Options" component={Options} />
+  </MainStack.Navigator>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.blue,
-  },
-  content: {
-    paddingTop: screen.height * 0.1,
-  },
-  logoContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  logoBackground: {
-    width: screen.width / 0.45,
-    height: screen.width * 0.45,
-  },
-  logo: {
-    position: 'absolute',
-    width: screen.width * 0.25,
-    height: screen.width * 0.25,
-  },
-  textHeader: {
-    color: colors.white,
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 14,
-    color: colors.white,
-    textAlign: 'center',
-  },
-  inputContainer: {
-    marginBottom: 10,
-  },
-  header: {
-    alignItems: 'flex-end',
-    marginHorizontal: 20,
-  },
-});
-
-export default ({ navigation }) => {
-  const baseCurrency = 'USD';
-  const quoteCurrency = 'GBP';
-  const conversionRate = 0.89824;
-  const date = '2020-03-23';
-
-  const [scrollEnabled, setScrollEnabled] = useState(false);
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
-      <ScrollView scrollEnabled={scrollEnabled}>
-        <SafeAreaView style={styles.header}>
-          <TouchableOpacity onPress={() => navigation.push('Options')}>
-            <Entypo name="cog" size={32} color={colors.white} />
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator mode="modal">
+    <ModalStack.Screen
+      name="Main"
+      component={MainStackScreen}
+      options={{ headerShown: false }}
+    />
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList}
+      options={({ navigation, route }) => ({
+        title: route.params && route.params.title,
+        headerLeft: null,
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.pop()}
+            style={{ paddingHorizontal: 10 }}
+          >
+            <Entypo name="cross" size={30} color={colors.blue} />
           </TouchableOpacity>
-        </SafeAreaView>
+        )
+      })}
+    />
+  </ModalStack.Navigator>
+);
 
-        <View style={styles.content}>
-          <View style={styles.logoContainer}>
-            <Image
-              source={require('../assets/images/background.png')}
-              style={styles.logoBackground}
-              resizeMode="contain"
-            />
-            <Image
-              source={require('../assets/images/logo.png')}
-              style={styles.logo}
-              resizeMode="contain"
-            />
-          </View>
-          <Text style={styles.textHeader}>Currency Converter</Text>
-          <View style={styles.inputContainer}>
-            <ConversionInput
-              text={baseCurrency}
-              value="123"
-              onButtonPress={() =>
-                navigation.push('CurrencyList', { title: 'Base Currency' })
-              }
-              keyboardType="numeric"
-              onChangeText={(text) => console.log('text', text)}
-            />
-            <ConversionInput
-              text={quoteCurrency}
-              value="123"
-              editable={false}
-              onButtonPress={() =>
-                navigation.push('CurrencyList', { title: 'Quote Currency' })
-              }
-            />
-          </View>
-          <Text style={styles.text}>
-            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(
-              new Date(date),
-              'MMM do, yyyy'
-            )}`}
-          </Text>
-          <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
-          <KeyboardSpacer onToggle={(visible) => setScrollEnabled(visible)} />
-        </View>
-      </ScrollView>
-    </View>
-  );
-};
+export default () => (
+  <NavigationContainer>
+    <ModalStackScreen />
+  </NavigationContainer>
+);
